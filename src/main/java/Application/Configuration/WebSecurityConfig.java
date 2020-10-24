d5bd8f81cd74ac0ff5bc072adf8e47c9515d1406
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-10-24 12:18:22 +0300

    Testing connection

--- src/main/java/Application/Configuration/WebSecurityConfig.java
+++ src/main/java/Application/Configuration/WebSecurityConfig.java
@@ -18,8 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.
+                cors().and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
